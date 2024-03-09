f118603e8244a6f4af7273b33b552342da62a013
Josh Cough <joshcough@gmail.com>
2024-03-09 11:50:39 -0600

    temporary hack

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import os
 from app.trollabot.trollabot import run_via_external_db, run_via_test_container
 
 def main():
-    print("Welcome to Trollabot 2.0")
-    if os.getenv("ENV") == "local":
-        run_via_test_container()
-    else:
-        run_via_external_db()
+    while True:
+        print("hello")
+        time.sleep(10)
+    # print("Welcome to Trollabot 2.0")
+    # if os.getenv("ENV") == "local":
+    #     run_via_test_container()
+    # else:
+    #     run_via_external_db()
 
 if __name__ == '__main__':
     main()
