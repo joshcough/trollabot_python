b44cb9802fd48020a808be085afb16e7d1c1cf09
Josh Cough <joshcough@gmail.com>
2024-03-09 12:17:03 -0600

    deployment ready cleanups.

--- app/trollabot/trollabot.py
+++ app/trollabot/trollabot.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 import os
+import signal
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -9,21 +10,22 @@ from app.trollabot.database import Base, DB_API
 from app.trollabot.irc_bot import IrcConfig, TwitchIRCBot, setup_connection
 from app.trollabot.messages import ChannelName
 
-def run_with_pg_connection_string(conn_str) -> None:
+def run_with_pg_connection_string(conn_str, on_engine_create) -> None:
     if conn_str.startswith("postgres://"):
         conn_str = conn_str.replace("postgres://", "postgresql://", 1)
 
     engine = create_engine(conn_str, isolation_level="REPEATABLE READ")
+    on_engine_create(engine)
     Base.metadata.create_all(engine)
-    TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-    db_session = TestingSessionLocal()
+    db_session = sessionmaker(autocommit=False, autoflush=False, bind=engine)()
     db_api: DB_API = DB_API(db_session)
 
-    # TODO: remove these before deploying
-    # but not until we write the !join command
     db_api.streams.join(ChannelName("artofthetroll"), "artofthetroll")
+    db_session.commit()
 
     reactor, connection = setup_connection(irc_config=IrcConfig())
+    make_shutdown_handler(reactor, db_session)
+
     bot: TwitchIRCBot = TwitchIRCBot(connection, db_api)
     reactor.process_forever()
 
@@ -32,7 +34,18 @@ def run_with_pg_connection_string(conn_str) -> None:
 
 def run_via_test_container() -> None:
     with PostgresContainer("postgres:latest") as postgres:
-        run_with_pg_connection_string(postgres.get_connection_url())
+        run_with_pg_connection_string(postgres.get_connection_url(),
+                                      lambda engine: Base.metadata.create_all(engine))
 
 def run_via_external_db() -> None:
-    run_with_pg_connection_string(os.getenv('DATABASE_URL'))
+    run_with_pg_connection_string(os.getenv('DATABASE_URL'), lambda engine: None)
+
+def make_shutdown_handler(db_session, reactor):
+    def shutdown_handler(signum, frame):
+        print('Shutting down gracefully...')
+        db_session.close()
+        # Optionally, also stop the reactor if you have access to it here
+        reactor.stop()
+
+    signal.signal(signal.SIGTERM, shutdown_handler)
+    signal.signal(signal.SIGINT, shutdown_handler)
--- main.py
+++ main.py
@@ -3,14 +3,11 @@ import os
 from app.trollabot.trollabot import run_via_external_db, run_via_test_container
 
 def main():
-    while True:
-        print("hello")
-        time.sleep(10)
-    # print("Welcome to Trollabot 2.0")
-    # if os.getenv("ENV") == "local":
-    #     run_via_test_container()
-    # else:
-    #     run_via_external_db()
+    print("Welcome to Trollabot 2.0")
+    if os.getenv("ENV") == "local":
+        run_via_test_container()
+    else:
+        run_via_external_db()
 
 if __name__ == '__main__':
     main()
