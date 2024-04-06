bb03021b3d9120b65de4db027b35ba2b35ae6cf9
Josh Cough <joshcough@gmail.com>
2024-04-06 14:18:40 -0500

    added a score page!

--- tests/web/test_web.py
+++ tests/web/test_web.py
@@ -5,8 +5,14 @@ test_user: str = "web_test_stream"
 
 def test_home_page(web_client, db_api, clean_db):
     """Test the home page route."""
+    response = web_client.get('/')
+    assert response.status_code == 200
+    assert b"Welcome" in response.data
+
+def test_score_page(web_client, db_api, clean_db):
+    """Test the score page route."""
     db_api.streams.insert_stream(test_stream, test_user)
     db_api.scores.upsert_score_all(test_stream, "joe", 69, "jim", 42)
-    response = web_client.get('/?channel_name=web_test_stream')
+    response = web_client.get('/score?channel_name=web_test_stream')
     assert response.status_code == 200
     assert b"joe 69 - 42 jim" in response.data
--- web/routes.py
+++ web/routes.py
@@ -6,7 +6,11 @@ bp = Blueprint('home', __name__)
 
 @bp.route('/')
 def home():
+    return render_template('home.html')
+
+@bp.route('/score')
+def score():
     channel_name = request.args.get('channel_name', 'artofthetroll')
     chan = ChannelName(channel_name)
-    score = g.db_api.scores.get_score(chan)
-    return render_template('home.html', score=score)
+    s = g.db_api.scores.get_score(chan)
+    return render_template('score.html', score=s)
